0cdb730d57e7f6fb67cb5c9e3c269c2d725437ff
Michael Mailman <mmailman@gmail.com>
2016-05-26 13:30:37 -0700

    Adding comments to the articleController.js

--- submissions/mike-peter/scripts/controllers/articleController.js
+++ submissions/mike-peter/scripts/controllers/articleController.js
@@ -8,47 +8,66 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // The route is when you hit /article/:id for this method
   articlesController.loadById = function(ctx, next) {
-    var articleData = function(article) {
+    //this function is passed in as a callback function to the Article.findWhere method
+    var articleData = function(article) { //article parameter is an array containing an object, and the object is the actual artcle data
       ctx.articles = article;
+
+      // this next() call, calls the next callback function in the route articlesController.index
       next();
     };
 
+    //this method does a webDB call to select data based on id and the ctx id
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // The route is when you hit /author/:authorName for this method
   articlesController.loadByAuthor = function(ctx, next) {
-    var authorData = function(articlesByAuthor) {
+    //This function is also passed into the Article.findWhere method
+    var authorData = function(articlesByAuthor) { // is an array of matching article objects by author
       ctx.articles = articlesByAuthor;
+
+      // this next() call, calls the next callback function in the route articlesController.index
       next();
     };
-
+    //this method does a webDB call to select data based on author and the ctx authorName
     Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // The route is when you hit /category/:categoryName for this method
   articlesController.loadByCategory = function(ctx, next) {
-    var categoryData = function(articlesInCategory) {
+    //This function is also passed into the Article.findWhere method
+    var categoryData = function(articlesInCategory) { // is an array of matching article objects by category
       ctx.articles = articlesInCategory;
+
+      // this next() call, calls the next callback function in the route articlesController.index
       next();
     };
 
+    //this method does a webDB call to select data based on category and the ctx categoryName
     Article.findWhere('category', ctx.params.categoryName, categoryData);
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  // The route is when you hit '/' for this method or if the default value is selected on the category or object filter
   articlesController.loadAll = function(ctx, next) {
-    var articleData = function(allArticles) {
+    var articleData = function(allArticles) { //This parameter is not necessary we are assigning a globally available array to the ctx here.
       ctx.articles = Article.all;
+
+      // this next() call, calls the next callback function in the route articlesController.index
       next();
     };
 
-    if (Article.all.length) {
+    if (Article.all.length) { //If array has data assign the arry to articles context
       ctx.articles = Article.all;
+
+      // this next() call, calls the next callback function in the route articlesController.index
       next();
     } else {
-      Article.fetchAll(articleData);
+      Article.fetchAll(articleData); //If array is empty the Article.fetchAll method is executed.
     }
   };
 
